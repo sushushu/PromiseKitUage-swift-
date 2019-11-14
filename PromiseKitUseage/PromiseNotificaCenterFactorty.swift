68d048c32d64ac02dded27c4ec2ea46fa436a0e8
sujh <pain_su@aiagain.com>
2019-11-14 20:55:29 +0800

    first commit

---
+++ PromiseKitUseage/PromiseNotificaCenterFactorty.swift
@@ -0,0 +1,9 @@
+//
+//  PromiseNotificaCenterFactorty.swift
+//  PromiseKitUseage
+//
+//  Created by Aiagain on 2019/11/14.
+//  Copyright © 2019 Aiagain. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# PromiseKitUage-swift-
