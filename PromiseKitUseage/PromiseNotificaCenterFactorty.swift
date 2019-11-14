847042db1ff725e78f53dd16826cde520c0d35d0
sujh <pain_su@aiagain.com>
2019-11-14 20:56:39 +0800

    init

--- PromiseKitUseage/PromiseNotificaCenterFactorty.swift
+++ PromiseKitUseage/PromiseNotificaCenterFactorty.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+class PromiseNotificaCenterFactorty : NSObject {
+    var name:String = ""
+    
+    init(name:String){
+        super.init()
+        
+        self.name = name
+        
+        //开始观察
+        beginObserver()
+    }
+    
+    //开始观察
+    func beginObserver() {
+        // 监听名字为kNotificationKey的通知
+        NotificationCenter.default.observe(once: Notification.Name(rawValue: "kNotificationKey")).done { notification in
+            // TODO: get notification, do your job
+            let userInfo = notification.userInfo as! [String: AnyObject]
+            if (userInfo.isEmpty) {
+                return;
+            }
+            
+            if ((userInfo["value1"]) != nil) {
+                print("\(self.name) 获取到通知，value1数据是 \(userInfo["value1"]!)")
+            }
+            
+            if ((userInfo["value2"]) != nil) {
+                print("\(self.name) 获取到通知，value2数据是 \(userInfo["value2"]!)")
+            }
+            
+            print("\(self.name) 执行完毕\n\n\n")
+            
+            // 需要注意Promise的做法只会观察一次，如果需要继续观察，要手动再次注册self.beginObserver()
+            self.beginObserver()
+        }
+    }
+}
+
--- PromiseKitUseage/ViewController.swift
+++ PromiseKitUseage/ViewController.swift
@@ -7,14 +7,122 @@
 //
 
 import UIKit
+import PromiseKit
+
 
 class ViewController: UIViewController {
-
+    
+    @objc var m_button = UIButton.init(type: .custom)
+    @objc dynamic var m_titleNum = 1 // 必须要加@objc关键词以及对象标记为dynamic,因为Swift 4不再自动给NSObject的子类添加@objec
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ui_init()
+        
+        // TODO: 以下三种方法对应三种拓展，直接调用即可
+        
+//        PromiseNSURLSessionrDemo()
+        
+        //        urlSessionGet()
+        
+        PromiseKVODemo()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        m_titleNum -= 1
+        m_button.setTitle(String(m_titleNum), for: .normal)
+    }
+    
+    
+    /// PromiseKit Foundation拓展之NSNotificationCenter的使用
+    private func PromiseNotificaCenterDemo() {
+        // 先批量注册通知
+        let _ = [PromiseNotificaCenterFactorty(name: "第0个通知") , PromiseNotificaCenterFactorty(name: "第1个通知") ,  PromiseNotificaCenterFactorty(name: "第2个通知")]
+        
+        // 开始发起通知
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "kNotificationKey"), object: self,
+                                        userInfo: ["value1":"aigain.com", "value2" : 8600])
+    }
+    
+
+    /// PromiseKit Foundation拓展之NSURLSession的使用
+    private func PromiseNSURLSessionrDemo() {
+        //创建URLRequest对象
+        let request = URLRequest(url: URL(string:"https://httpbin.org/get?foo=bar")!)
+        
+        // 原生的网络请求
+        let task = URLSession.shared.dataTask(with: request) { ( data, urlRespone, error) in
+            if !(data == nil) && (error == nil)  {
+                print(self.unPack(data: data!))
+            }
+        }
+        task.resume()
+        
+        
+        // Promise的做法
+        _ = URLSession.shared.dataTask(.promise, with: request)
+            .validate()
+            .done { data, response in
+                print(self.unPack(data: data))
+        }
+    }
+    
+    
+    /// PromiseKit Foundation拓展之KVO的使用
+    private func PromiseKVODemo() {
+        Timer.scheduledTimer(timeInterval: 1, target: self,
+                             selector: #selector(changeMessage),
+                             userInfo: nil, repeats: true)  //定时器
+        
+
+        // 系统原生的做法
+        self.addObserver(self, forKeyPath: "m_titleNum", options: .new, context: nil)
+        
+        // promise的做法
+        PromiseObserve()
+    }
+    
+    // 系统原生的做法,监听
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        print("系统原生的做法监听到了 \(change![NSKeyValueChangeKey.newKey]!)")
     }
 
-
+    // promise的做法
+    func PromiseObserve() {
+        self.observe(.promise, keyPath: #keyPath(m_titleNum)).done { value in
+            print(" PromiseObserve + \(value ?? "") ")
+            //继续观察
+            self.PromiseObserve()
+        }
+    }
+    
+    
+    private func unPack(data:Data) -> Any {
+        return try! JSONSerialization.jsonObject(with: data as Data, options:.mutableContainers)
+    }
+    
+    @objc func changeMessage() {
+        m_titleNum += 1
+        m_button.setTitle(String(m_titleNum), for: .normal)
+        print("定时器： \(m_titleNum)")
+    }
+     
+    private func ui_init() {
+        m_button.layer.cornerRadius = 10
+        m_button.backgroundColor = UIColor.systemTeal
+        m_button.frame = CGRect.init(x: 100, y: 200, width: 100, height: 100)
+        m_button.setTitle("0", for: .normal)
+        m_button.addTarget(self, action: #selector(onClick), for: .touchUpInside)
+        self.view.addSubview(m_button)
+    }
+    
+    @objc private func onClick() {
+        m_titleNum += 1
+        m_button.setTitle(String(m_titleNum), for: .normal)
+    }
 }
+
+
+
 
